274b59c09bc5dc62c8ace2b65d2ac842c3922a09
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2023-02-11 21:21:29 +0700

    Add skeleton + get meeting data

--- src/pages/Activities/Meetings/MeetingsPage.jsx
+++ src/pages/Activities/Meetings/MeetingsPage.jsx
@@ -1,11 +1,10 @@
+import Skeleton from "../../../components/Skeleton/Skeleton.jsx";
 import useSheetAPI from "../../../services/google/hooks/useSheetAPI.js";
 import { joinCls, randomImgUrl } from "../../../utilities/text.utils.js";
 
 import style from "./style.module.css";
 export default function MeetingsPage() {
-  const { isLoading, data: meetings, getMore: getMoreMeeting } = useSheetAPI("Meeting", "A", "F", { earlyTake: 8 });
-
-  console.log(meetings);
+  const { isLoading, isFull, data: meetings, getMore: getMoreMeeting } = useSheetAPI("Meeting", "K", "P", { earlyTake: 8 });
 
   return (
     <div>
@@ -24,32 +23,76 @@ export default function MeetingsPage() {
           </div>
         </div>
         <h1 className="display-5 f-montserrat fw-bolder text-center text-gradient text-uppercase my-5">meetings</h1>
-        <div id="team-meeting" className="container justify-content-around">
-          <div className="row">
-            <a href="#" className={joinCls("col-5 text-decoration-none", style["post"])}>
-              <div className="row justify-content-around">
-                <div className="col-5 p-0">
-                  <img src={randomImgUrl(500)} alt="" className={style["post-image"]} />
+        <div id="team-meeting" className="container">
+          <div className="row g-5">
+            {meetings.map(([id, title, description, date, imgUrl, contentUrl]) => (
+              <div className="col-6">
+                <a key={id} href={contentUrl} className={joinCls("text-decoration-none", style["post"])}>
+                  <div className="row justify-content-around">
+                    <div className="col-5 p-0">
+                      <img src={imgUrl} alt="" className={style["post-image"]} />
+                    </div>
+                    <div className="col-6">
+                      <div className={joinCls("text-ellipsis f-montserrat fw-bold text-black", style["post-title"])}>{title}</div>
+                      <div className={joinCls("text-ellipsis", style["post-description"])}>{description}</div>
+                      <div className={joinCls("text-uppercase mt-2", style["post-date"])}>{date}</div>
+                      <div className="text-decoration-none d-flex align-items-center mt-1">
+                        <p className="m-0 fw-bold text-gradient">Read more</p>
+                        <p className={joinCls("mb-0 mt-1 ms-2 d-flex text-decoration-none align-items-center justify-content-center text-gradient", style["read-btn"])}>
+                          <i className="fa-solid fa-arrow-right"></i>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            ))}
+
+            {isLoading && (
+              <>
+                <div className="col-6">
+                  <div className={joinCls("text-decoration-none", style["post"])}>
+                    <div className="row justify-content-around">
+                      <div className="col-5">
+                        <Skeleton height="12rem" className="rounded" />
+                      </div>
+                      <div className="col-6">
+                        <Skeleton className="mb-3" height="3rem" />
+                        <Skeleton className="mb-3" />
+                        <Skeleton className="mb-3" />
+                        <Skeleton className="mb-3" />
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className="col-6">
-                  <div className={joinCls("text-ellipsis f-montserrat fw-bold text-black", style["post-title"])}>
-                    Lorem ipsum dolor sit amet,consectetur adipiscing elit. Nunc vulputate libero
-                  </div>
-                  <div className={joinCls("text-ellipsis", style["post-description"])}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero,Nunc vulputate libero Nunc vulputate libero,Nunc vulputate libero Nunc vulputate
-                    libero,Nunc vulputate libero
-                  </div>
-                  <div className={joinCls("text-uppercase mt-2", style["post-date"])}>May 25, 2023</div>
-                  <div className="text-decoration-none d-flex align-items-center mt-1">
-                    <p className="m-0 fw-bold text-gradient">Read more</p>
-                    <p className={joinCls("mb-0 mt-1 ms-2 d-flex text-decoration-none align-items-center justify-content-center text-gradient", style["read-btn"])}>
-                      <i className="fa-solid fa-arrow-right"></i>
-                    </p>
+                  <div className={joinCls("text-decoration-none", style["post"])}>
+                    <div className="row justify-content-around">
+                      <div className="col-5">
+                        <Skeleton height="12rem" className="rounded" />
+                      </div>
+                      <div className="col-6">
+                        <Skeleton className="mb-3" height="3rem" />
+                        <Skeleton className="mb-3" />
+                        <Skeleton className="mb-3" />
+                        <Skeleton className="mb-3" />
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </a>
+              </>
+            )}
           </div>
+
+          {!isFull && (
+            <div className="row justify-content-center mt-5">
+              <div className="col-auto">
+                <button className={joinCls("btn btn-lg btn-outline-gradient rounded-pill", style["see-more-btn"])} onClick={() => getMoreMeeting(8)}>
+                  SEE MORE
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
