c7ba687bc05246b28389c2c1787d1efd0cf51328
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2023-02-19 11:24:32 +0700

    Update sheet hook API

--- src/pages/Teams/TeamsPage.jsx
+++ src/pages/Teams/TeamsPage.jsx
@@ -6,15 +6,25 @@ import Modal from "../../components/Modal/Modal";
 import Slider from "../../components/Slider/Slider";
 import { joinCls, randomImgUrl } from "../../utilities/text.utils";
 import { MEETINGS_ACTIVITY_PAGE_PATH } from "../Activities/Meetings/constants";
-import { TEAMS_PAGE_PATH } from "./constants";
+import { BANANA, HEROES_COMPANY, NIFFLER, POWER_RANGERS, TEAMS_PAGE_PATH } from "./constants";
 import Footer from "../../components/Footer/Footer";
 
 import style from "./style.module.css";
+import useSheetAPI from "../../services/google/hooks/useSheetAPI";
+
+const sheetNameMap = {
+  [BANANA]: "Banana",
+  [HEROES_COMPANY]: "HeroesCompany",
+  [POWER_RANGERS]: "PowerRangers",
+  [NIFFLER]: "Niffler",
+};
 
 export default function TeamsPage() {
   const {
     params: { name },
   } = useMatch(TEAMS_PAGE_PATH);
+
+  const { isLoading: isEventLoading, isFull: isEventFull, data: events, getMore: getMoreEvent } = useSheetAPI(sheetNameMap[name], "K", "P", { earlyTake: 8 });
 
   const exploreCategories = [
     {
@@ -54,27 +64,14 @@ export default function TeamsPage() {
   return (
     <div className="bg-black">
       <section
-        className={joinCls(
-          "container-fluid d-flex flex-column justify-content-end align-items-center",
-          style["header"]
-        )}
+        className={joinCls("container-fluid d-flex flex-column justify-content-end align-items-center", style["header"])}
         style={{ backgroundImage: `url(${randomImgUrl()})` }}
       >
         <div className={style["fade"]}></div>
-        <div
-          className={joinCls(
-            "d-flex flex-column align-items-center",
-            style["overlay"]
-          )}
-        >
+        <div className={joinCls("d-flex flex-column align-items-center", style["overlay"])}>
           <h1>POWER RANGERS</h1>
           <p>"Justice For All"</p>
-          <button
-            className={joinCls(
-              "btn d-flex justify-content-center align-items-center rounded-circle",
-              style["icon"]
-            )}
-          >
+          <button className={joinCls("btn d-flex justify-content-center align-items-center rounded-circle", style["icon"])}>
             <span className="fa-solid fa-xl fa-chevron-down"></span>
           </button>
         </div>
@@ -85,13 +82,9 @@ export default function TeamsPage() {
           Explore <span className="text-primary">Power Rangers</span>
         </h1>
         <p className="text-light text-center fst-italic fs-5 mt-5">
-          "Each Power Rangers's member has an unique color and that creates an
-          unique team. But we are always willing to support and assist all
-          others to overcome all difficulties, strive to conquer themselves and
-          create meaningful values for society. The special thing about the PRs
-          team is that we always respect, sympathize and share together to
-          create a sense of solidarity so that everyone is always happy and
-          closer to each other."
+          "Each Power Rangers's member has an unique color and that creates an unique team. But we are always willing to support and assist all others to overcome all difficulties,
+          strive to conquer themselves and create meaningful values for society. The special thing about the PRs team is that we always respect, sympathize and share together to
+          create a sense of solidarity so that everyone is always happy and closer to each other."
         </p>
         <div className="row mt-5 gap-5 text-white justify-content-center">
           <div className="col-auto">
@@ -113,22 +106,11 @@ export default function TeamsPage() {
         <div className="row gx-5">
           {exploreCategories.map(({ title, description, images }) => (
             <div key={title} className="col-3">
-              <div
-                className={joinCls(
-                  "position-relative overflow-hidden cursor-pointer",
-                  style["explore-img-area"]
-                )}
-                onClick={() => handleShownExploreImages(images)}
-              >
-                <Image
-                  src={randomImgUrl()}
-                  className="d-flex justify-content-center align-items-end rounded"
-                />
+              <div className={joinCls("position-relative overflow-hidden cursor-pointer", style["explore-img-area"])} onClick={() => handleShownExploreImages(images)}>
+                <Image src={randomImgUrl()} className="d-flex justify-content-center align-items-end rounded" />
                 <div className="position-absolute bottom-0 w-100">
                   <div className="position-relative w-100 mx-2">
-                    <h5 className="position-absolute text-white text-center w-100 mb-2">
-                      {title}
-                    </h5>
+                    <h5 className="position-absolute text-white text-center w-100 mb-2">{title}</h5>
                     <p className="text-white text-center mb-3">{description}</p>
                   </div>
                 </div>
@@ -139,9 +121,7 @@ export default function TeamsPage() {
       </section>
 
       <section className="container mt-10">
-        <h1 className="display-6 f-montserrat fw-bolder text-white text-center">
-          Recent Activities
-        </h1>
+        <h1 className="display-6 f-montserrat fw-bolder text-white text-center">Recent Activities</h1>
         <Slider
           items={sliderItems}
           className="gap-3 mt-5"
@@ -149,29 +129,16 @@ export default function TeamsPage() {
             <Link
               key={item.id}
               to={MEETINGS_ACTIVITY_PAGE_PATH}
-              className={joinCls(
-                "card text-decoration-none text-bg-dark border-0 rounded-3 overflow-hidden",
-                style["activity-item"]
-              )}
+              className={joinCls("card text-decoration-none text-bg-dark border-0 rounded-3 overflow-hidden", style["activity-item"])}
             >
               <Image src={item.url} className="card-img" />
               <div className="card-img-overlay d-flex flex-column justify-content-end">
                 <div className="position-relative">
-                  <div
-                    className={joinCls(
-                      "position-absolute",
-                      style["card-header"]
-                    )}
-                  >
-                    <h3 className="badge text-success text-bg-light text-uppercase px-2 py-2">
-                      Special title treatment
-                    </h3>
+                  <div className={joinCls("position-absolute", style["card-header"])}>
+                    <h3 className="badge text-success text-bg-light text-uppercase px-2 py-2">Special title treatment</h3>
                     <h5 className="card-text">Traveling</h5>
                   </div>
-                  <p className={joinCls("card-text", style["card-sub-text"])}>
-                    With supporting text below as a natural lead-in to
-                    additional content
-                  </p>
+                  <p className={joinCls("card-text", style["card-sub-text"])}>With supporting text below as a natural lead-in to additional content</p>
                 </div>
                 <p className="card-text text-secondary fw-bold">
                   <small>May 30, 2022</small>
@@ -191,32 +158,13 @@ export default function TeamsPage() {
 
         <div className="position-relative w-100 vh-100 mt-10">
           <Image src={randomImgUrl()} className="w-100 h-100" />
-          <div
-            className={joinCls(
-              "position-absolute w-100 top-0",
-              style["fade-down"]
-            )}
-          ></div>
-          <div
-            className={joinCls(
-              "position-absolute w-100 bottom-0",
-              style["fade-up"]
-            )}
-          ></div>
+          <div className={joinCls("position-absolute w-100 top-0", style["fade-down"])}></div>
+          <div className={joinCls("position-absolute w-100 bottom-0", style["fade-up"])}></div>
         </div>
       </section>
 
-      <Modal
-        className="modal-xl"
-        contentClassName="bg-transparent"
-        isShown={isShownModal}
-        onClose={() => setIsShownModal(false)}
-      >
-        <Masonry
-          images={currentExploreImages}
-          renderItem={(image) => <Image key={image} src={image} />}
-          gutter={4}
-        />
+      <Modal className="modal-xl" contentClassName="bg-transparent" isShown={isShownModal} onClose={() => setIsShownModal(false)}>
+        <Masonry images={currentExploreImages} renderItem={(image) => <Image key={image} src={image} />} gutter={4} />
       </Modal>
     </div>
   );
