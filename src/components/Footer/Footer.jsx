cc629a97f122f5b473936a4837ecc53ce5af3748
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2023-03-04 18:09:00 +0700

    update aboutus

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -19,273 +19,162 @@ import style from "./style.module.css";
 import { DARK_THEME, LIGHT_THEME } from "../../services/theme/constants";
 
 export default function Footer({ theme = LIGHT_THEME, className, ...props }) {
-  const dispatch = useDispatch();
-  const locale = useSelector((state) => state.language.locale, shallowEqual);
+	const dispatch = useDispatch();
+	const locale = useSelector((state) => state.language.locale, shallowEqual);
 
-  const containerRef = useRef();
-  const [sparks, setSparks] = useState([]);
+	const containerRef = useRef();
+	const [sparks, setSparks] = useState([]);
 
-  const handleSparkTransitionEnded = (key, event) => {
-    const container = event.target.parentElement;
-    const newSpark = generateSpark(
-      { from: 64, to: container.clientWidth - 64 },
-      { from: -540, to: -540 },
-      { from: -40, to: 0 },
-      { from: 2.4, to: 5 },
-      { from: 0, to: 360 },
-      { from: 4, to: 16 }
-    );
+	const handleSparkTransitionEnded = (key, event) => {
+		const container = event.target.parentElement;
+		const newSpark = generateSpark(
+			{ from: 64, to: container.clientWidth - 64 },
+			{ from: -540, to: -540 },
+			{ from: -40, to: 0 },
+			{ from: 2.4, to: 5 },
+			{ from: 0, to: 360 },
+			{ from: 4, to: 16 }
+		);
 
-    const sparkIndex = sparks.findIndex((spark) => spark.key === key);
-    setSparks([
-      ...sparks.slice(0, sparkIndex),
-      ...sparks.slice(sparkIndex + 1),
-      newSpark,
-    ]);
-  };
+		const sparkIndex = sparks.findIndex((spark) => spark.key === key);
+		setSparks([...sparks.slice(0, sparkIndex), ...sparks.slice(sparkIndex + 1), newSpark]);
+	};
 
-  useEffect(() => {
-    const container = containerRef.current;
-    setSparks(
-      [...Array(10)].map((item) =>
-        generateSpark(
-          { from: 64, to: container.clientWidth - 64 },
-          { from: -540, to: -540 },
-          { from: -40, to: 0 },
-          { from: 2.4, to: 5 },
-          { from: 0, to: 360 },
-          { from: 4, to: 16 }
-        )
-      )
-    );
-  }, []);
+	useEffect(() => {
+		const container = containerRef.current;
+		setSparks(
+			[...Array(10)].map((item) =>
+				generateSpark({ from: 64, to: container.clientWidth - 64 }, { from: -540, to: -540 }, { from: -40, to: 0 }, { from: 2.4, to: 5 }, { from: 0, to: 360 }, { from: 4, to: 16 })
+			)
+		);
+	}, []);
 
-  const themeContent = {
-    [DARK_THEME]: {
-      logo: HugoLogoLightSvg,
-      titleColor: "text-white",
-      textColor: "text-light",
-      languageBtn: "btn-outline-light",
-    },
-    [LIGHT_THEME]: {
-      logo: HugoLogoDarkSvg,
-      titleColor: "text-black",
-      textColor: "text-dark",
-      languageBtn: "btn-outline-dark",
-    },
-  };
+	const themeContent = {
+		[DARK_THEME]: {
+			logo: HugoLogoLightSvg,
+			titleColor: "text-white",
+			textColor: "text-light",
+			languageBtn: "btn-outline-light",
+		},
+		[LIGHT_THEME]: {
+			logo: HugoLogoDarkSvg,
+			titleColor: "text-black",
+			textColor: "text-dark",
+			languageBtn: "btn-outline-dark",
+		},
+	};
 
-  return (
-    <section
-      ref={containerRef}
-      className={joinCls("position-relative", className)}
-      {...props}
-    >
-      <div
-        className={joinCls(
-          "position-absolute bottom-100 left-0 w-100 border-bottom border-2 overflow-hidden",
-          style["sparks-area"]
-        )}
-      >
-        <div className="position-relative w-100 h-100">
-          {sparks.map(
-            ({ key, x, yStart, yEnd, size, rotateEnd, transitionTime }) => (
-              <Spark
-                key={key}
-                x={x}
-                yStart={yStart}
-                yEnd={yEnd}
-                size={size}
-                rotateEnd={rotateEnd}
-                transitionTime={transitionTime}
-                onTransitionEnd={(event) =>
-                  handleSparkTransitionEnded(key, event)
-                }
-                sparkComponent={FooterSpark}
-              />
-            )
-          )}
-        </div>
-      </div>
+	return (
+		<section ref={containerRef} className={joinCls("position-relative", className)} {...props}>
+			<div className={joinCls("position-absolute bottom-100 left-0 w-100 border-bottom border-2 overflow-hidden", style["sparks-area"])}>
+				<div className="position-relative w-100 h-100">
+					{sparks.map(({ key, x, yStart, yEnd, size, rotateEnd, transitionTime }) => (
+						<Spark
+							key={key}
+							x={x}
+							yStart={yStart}
+							yEnd={yEnd}
+							size={size}
+							rotateEnd={rotateEnd}
+							transitionTime={transitionTime}
+							onTransitionEnd={(event) => handleSparkTransitionEnded(key, event)}
+							sparkComponent={FooterSpark}
+						/>
+					))}
+				</div>
+			</div>
 
-      <div className={joinCls("position-relative py-5", style["footer"])}>
-        <div className="container">
-          <div className="row">
-            <div className="col-3">
-              <div className="d-flex flex-column">
-                <Link to="#">
-                  <Image
-                    src={themeContent[theme].logo}
-                    width={240}
-                    className={joinCls("px-2 mb-2")}
-                  />
-                </Link>
-                <p
-                  className={joinCls(
-                    "mt-3 mb-0",
-                    themeContent[theme].textColor
-                  )}
-                >
-                  This website is a product of Hugo IT club and part of the
-                  HUGOVERSARY project to celebrate the 15th birthday of Hugo
-                  English Club. This website is to store, update and promote the
-                  activities and events of Hugo English Club up to the present
-                  time.
-                </p>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="d-flex flex-column">
-                <h3
-                  className={joinCls(
-                    "text-uppercase mt-3 mb-4",
-                    themeContent[theme].titleColor
-                  )}
-                >
-                  About Us
-                </h3>
-                <Link
-                  to={ABOUT_US_PAGE_PATH}
-                  className={joinCls(
-                    "text-decoration-none mb-2",
-                    themeContent[theme].textColor
-                  )}
-                >
-                  Our Staffs
-                </Link>
-                <Link
-                  to={PROJECT_TEAM_PAGE_PATH}
-                  className={joinCls(
-                    "text-decoration-none mb-2",
-                    themeContent[theme].textColor
-                  )}
-                >
-                  HUGOVERSARY Team
-                </Link>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="d-flex flex-column">
-                <h3
-                  className={joinCls(
-                    "text-uppercase mt-3 mb-4",
-                    themeContent[theme].titleColor
-                  )}
-                >
-                  Contact
-                </h3>
-                <Link
-                  to="#"
-                  className={joinCls(
-                    "text-decoration-none mb-2",
-                    themeContent[theme].textColor
-                  )}
-                >
-                  Email: hugoclub.dut@gmail.com
-                </Link>
-                <Link
-                  to="#"
-                  className={joinCls(
-                    "text-decoration-none mb-2",
-                    themeContent[theme].textColor
-                  )}
-                >
-                  Phone: +84 981-770-874
-                </Link>
-                <div className="row">
-                  <Link
-                    to="https://www.facebook.com/HUGOClub"
-                    className={joinCls(
-                      "col-2 text-decoration-none fs-3 text-gradient",
-                      style["contact-icon"]
-                    )}
-                  >
-                    <i className="fa-brands fa-facebook"></i>
-                  </Link>
-                  <Link
-                    to="https://www.youtube.com/channel/UCmhyu5rOUvECgvA4YEEKnxg"
-                    className={joinCls(
-                      "col-2 text-decoration-none fs-3 text-gradient",
-                      style["contact-icon"]
-                    )}
-                  >
-                    <i className="fa-brands fa-youtube"></i>
-                  </Link>
-                  <Link
-                    to="https://www.instagram.com/hugoenglishclub/"
-                    className={joinCls(
-                      "col-2 text-decoration-none fs-3 text-gradient",
-                      style["contact-icon"]
-                    )}
-                  >
-                    <i class="fa-brands fa-instagram"></i>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="d-flex flex-column">
-                <h3
-                  className={joinCls(
-                    "text-uppercase mt-3 mb-4",
-                    themeContent[theme].titleColor
-                  )}
-                >
-                  Language
-                </h3>
-                <div className={joinCls("dropdown", style["language-btn"])}>
-                  <div
-                    className={joinCls(
-                      "btn dropdown-toggle d-flex gap-2 align-items-center",
-                      themeContent[theme].languageBtn
-                    )}
-                    data-bs-toggle="dropdown"
-                  >
-                    {locale === EN_LOCALE && (
-                      <>
-                        <Image src={AmericaFlagSvg} className="me-2" />
-                        <h6 className="mb-0">English</h6>
-                      </>
-                    )}
-                    {locale === VI_LOCALE && (
-                      <>
-                        <Image src={VietnamFlagSvg} className="me-2" />
-                        <h6 className="mb-0">Vietnamese</h6>
-                      </>
-                    )}
-                  </div>
-                  <ul className="dropdown-menu">
-                    <li
-                      className="dropdown-item d-flex gap-3 align-items-center"
-                      onClick={() => dispatch(changeLocale(EN_LOCALE))}
-                    >
-                      <Image src={AmericaFlagSvg} />
-                      <h6 className="mb-0">English</h6>
-                    </li>
-                    <li
-                      className="dropdown-item d-flex gap-3 align-items-center"
-                      onClick={() => dispatch(changeLocale(VI_LOCALE))}
-                    >
-                      <Image src={VietnamFlagSvg} />
-                      <h6 className="mb-0">Vietnamese</h6>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+			<div className={joinCls("position-relative py-5", style["footer"])}>
+				<div className="container">
+					<div className="row">
+						<div className="col-3">
+							<div className="d-flex flex-column">
+								<Link to="#">
+									<Image src={themeContent[theme].logo} width={240} className={joinCls("px-2 mb-2")} />
+								</Link>
+								<p className={joinCls("mt-3 mb-0", themeContent[theme].textColor)}>
+									This website is a product of Hugo IT club and part of the HUGOVERSARY project to celebrate the 15th birthday of Hugo English Club. This website is to store,
+									update and promote the activities and events of Hugo English Club up to the present time.
+								</p>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="d-flex flex-column">
+								<h3 className={joinCls("text-uppercase mt-3 mb-4", themeContent[theme].titleColor)}>About Us</h3>
+								<Link to={ABOUT_US_PAGE_PATH} className={joinCls("text-decoration-none mb-2", themeContent[theme].textColor)}>
+									Our Staffs
+								</Link>
+								<Link to={PROJECT_TEAM_PAGE_PATH} className={joinCls("text-decoration-none mb-2", themeContent[theme].textColor)}>
+									HUGOVERSARY Team
+								</Link>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="d-flex flex-column">
+								<h3 className={joinCls("text-uppercase mt-3 mb-4", themeContent[theme].titleColor)}>Contact</h3>
+								<Link to="#" className={joinCls("text-decoration-none mb-2", themeContent[theme].textColor)}>
+									Email: hugoclub.dut@gmail.com
+								</Link>
+								<Link to="#" className={joinCls("text-decoration-none mb-2", themeContent[theme].textColor)}>
+									Phone: +84 981-770-874
+								</Link>
+								<div className="row">
+									<a target="_blank" href="https://www.facebook.com/HUGOClub" className={joinCls("col-2 text-decoration-none fs-3 text-gradient", style["contact-icon"])}>
+										<i className="fa-brands fa-facebook"></i>
+									</a>
+									<a
+										target="_blank"
+										href="https://www.youtube.com/channel/UCmhyu5rOUvECgvA4YEEKnxg"
+										className={joinCls("col-2 text-decoration-none fs-3 text-gradient", style["contact-icon"])}
+									>
+										<i className="fa-brands fa-youtube"></i>
+									</a>
+									<a target="_blank" href="https://www.instagram.com/hugoenglishclub/" className={joinCls("col-2 text-decoration-none fs-3 text-gradient", style["contact-icon"])}>
+										<i class="fa-brands fa-instagram"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="d-flex flex-column">
+								<h3 className={joinCls("text-uppercase mt-3 mb-4", themeContent[theme].titleColor)}>Language</h3>
+								<div className={joinCls("dropdown", style["language-btn"])}>
+									<div className={joinCls("btn dropdown-toggle d-flex gap-2 align-items-center", themeContent[theme].languageBtn)} data-bs-toggle="dropdown">
+										{locale === EN_LOCALE && (
+											<>
+												<Image src={AmericaFlagSvg} className="me-2" />
+												<h6 className="mb-0">English</h6>
+											</>
+										)}
+										{locale === VI_LOCALE && (
+											<>
+												<Image src={VietnamFlagSvg} className="me-2" />
+												<h6 className="mb-0">Vietnamese</h6>
+											</>
+										)}
+									</div>
+									<ul className="dropdown-menu">
+										<li className="dropdown-item d-flex gap-3 align-items-center" onClick={() => dispatch(changeLocale(EN_LOCALE))}>
+											<Image src={AmericaFlagSvg} />
+											<h6 className="mb-0">English</h6>
+										</li>
+										<li className="dropdown-item d-flex gap-3 align-items-center" onClick={() => dispatch(changeLocale(VI_LOCALE))}>
+											<Image src={VietnamFlagSvg} />
+											<h6 className="mb-0">Vietnamese</h6>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
 
-          <div className="row justify-content-center">
-            <div className="col-auto">
-              <p
-                className={joinCls("opacity-25", themeContent[theme].textColor)}
-              >
-                Copyright © 2022 HUGO ENGLISH CLUB
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+					<div className="row justify-content-center">
+						<div className="col-auto">
+							<p className={joinCls("opacity-25", themeContent[theme].textColor)}>Copyright © 2022 HUGO ENGLISH CLUB</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/pages/AboutUs/AboutUsPage.jsx
+++ src/pages/AboutUs/AboutUsPage.jsx
@@ -6,129 +6,77 @@ import Image from "../../components/Image/Image.jsx";
 
 import style from "./style.module.css";
 export default function AboutUsPage() {
-  const {
-    isLoading: isAboutUsLoading,
-    isFull: isAboutUsFull,
-    data: staffMembers,
-    getMore: getMoreStaffs,
-  } = useSheetAPI("AboutUs", "M", "P", { earlyTake: 13 });
-  const {
-    isIntroductionLoading,
-    isIntroductionFull,
-    data: introductionContent,
-  } = useSheetAPI("AboutUs", "Q", "R", {
-    earlyTake: 1,
-  });
-  return (
-    <div>
-      <div
-        className={joinCls(
-          "position-absolute h-100 w-100",
-          style["about-us-bg"]
-        )}
-      >
-        <Image src={logoHugo} className="position-fixed" />
-      </div>
-      {introductionContent.map(([introduction, imgUrl]) => (
-        <div>
-          <div className="container">
-            <div
-              className={joinCls(
-                "row justify-content-between align-items-center"
-              )}
-            >
-              <p
-                className={joinCls(
-                  "col-lg-8 col-12 mt-5 fw-bold f-montserrat",
-                  style["about-introduction-title"]
-                )}
-              >
-                Our club built base on <br />
-                <span>LEARN, DEVELOP</span> <br />
-                and <span>JOY.</span>
-              </p>
-              <div
-                className={joinCls(
-                  "col-lg-9 col-12 my-5 f-lato text-justify",
-                  style["introduction-content"]
-                )}
-              >
-                {introduction}
-              </div>
-            </div>
-          </div>
-          <img alt="" src={imgUrl} className="w-100"></img>
-        </div>
-      ))}
-      <div id="staff-area" className="mt-5">
-        <div className="container">
-          <div
-            className={joinCls(
-              "pb-3 f-montserrat fw-bold text-uppercase text-center text-gradient d-flex justify-content-center",
-              style["about-human-title"]
-            )}
-          >
-            get to know <br /> hugo's human
-          </div>
-          <div
-            className={joinCls(
-              "text-center row justify-content-center",
-              style["about-human-description"]
-            )}
-          >
-            <p className={joinCls("mt-3 col-lg-6 col-8")}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-              vulputate libero et velit interdum, ac aliquet odio mattis. Class
-              aptent taciti sociosqu ad litora torquent per conubia nostra, per
-              inceptos himenaeos. Curabitur tempus urna at turpis condimentum
-              lobortis.
-            </p>
-          </div>
-          <div className="row">
-            {staffMembers.map(([id, name, imageUrl, title]) => (
-              <div
-                key={id}
-                className={joinCls(
-                  "col-lg-3 col-12 col-md-6 text-center",
-                  style["item"]
-                )}
-              >
-                <img
-                  src={imageUrl}
-                  alt=""
-                  style={{
-                    objectFit: "cover",
-                    width: "100%",
-                    height: "65%",
-                    borderRadius: "10px",
-                  }}
-                />
-                <p className={joinCls("mb-0 f-montserrat", style["mem-name"])}>
-                  {name}
-                </p>
-                <p
-                  className={joinCls(
-                    "f-montserrat text-uppercase",
-                    style["mem-title"]
-                  )}
-                >
-                  {title}
-                </p>
-                <p
-                  className={joinCls(
-                    "f-montserrat text-italic",
-                    style["mem-motto"]
-                  )}
-                >
-                  “Always smile as if you inhaled N2O, fly high like H2,
-                  sublimate like I2, strong-willed like N2 and gentle like H2O.”
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <Footer className="mt-10 bg-light" />
-    </div>
-  );
+	const { isLoading: isAboutUsLoading, isFull: isAboutUsFull, data: staffMembers, getMore: getMoreStaffs } = useSheetAPI("AboutUs", "N", "R", { earlyTake: 13 });
+	const {
+		isIntroductionLoading,
+		isIntroductionFull,
+		data: introductionContent,
+	} = useSheetAPI("AboutUs", "S", "T", {
+		earlyTake: 1,
+	});
+
+	console.log(staffMembers);
+
+	return (
+		<div>
+			<div className={joinCls("position-absolute h-100 w-100", style["about-us-bg"])}>
+				<Image src={logoHugo} className="position-fixed" />
+			</div>
+			{introductionContent.map(([introduction, imgUrl]) => (
+				<div>
+					<div className="container">
+						<div className={joinCls("row justify-content-between align-items-center")}>
+							<p className={joinCls("col-lg-8 col-12 mt-5 fw-bold f-montserrat", style["about-introduction-title"])}>
+								Our club built base on <br />
+								<span>LEARN, DEVELOP</span> <br />
+								and <span>JOY.</span>
+							</p>
+							<div className={joinCls("col-lg-9 col-12 my-5 f-lato text-justify", style["introduction-content"])}>{introduction}</div>
+						</div>
+					</div>
+					<img alt="" src={imgUrl} className="w-100"></img>
+				</div>
+			))}
+			<div className="position-relative container mt-5 z-1">
+				<div className={joinCls("pb-3 f-montserrat fw-bold text-uppercase text-center text-gradient d-flex justify-content-center", style["about-human-title"])}>
+					get to know <br /> hugo's human
+				</div>
+				<div className={joinCls("text-center row justify-content-center", style["about-human-description"])}>
+					<p className={joinCls("mt-3 col-lg-6 col-8")}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora
+						torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.
+					</p>
+				</div>
+				<div className="row justify-content-center align-items-stretch">
+					{staffMembers.slice(0, 1).map(([id, name, imageUrl, title, motto]) => (
+						<div key={id} className={joinCls("col-lg-4 col-12 col-md-6")}>
+							<div className={joinCls("text-center rounded-3 overflow-hidden h-100", style["item"])}>
+								<Image src={imageUrl} />
+								<div className="p-2">
+									<p className={joinCls("mb-0 f-montserrat", style["mem-name"])}>{name}</p>
+									<p className={joinCls("f-montserrat text-uppercase", style["mem-title"])}>{title}</p>
+									<p className={joinCls("f-montserrat text-italic", style["mem-motto"])}>{motto}</p>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+				<div className="row justify-content-center align-items-stretch g-4 mt-4">
+					{staffMembers.slice(1).map(([id, name, imageUrl, title, motto]) => (
+						<div key={id} className={joinCls("col-lg-3 col-12 col-md-6")}>
+							<div className={joinCls("text-center rounded-3 overflow-hidden h-100", style["item"])}>
+								<Image src={imageUrl} />
+								<div className="p-2">
+									<p className={joinCls("mb-0 f-montserrat", style["mem-name"])}>{name}</p>
+									<p className={joinCls("f-montserrat text-uppercase", style["mem-title"])}>{title}</p>
+									<p className={joinCls("f-montserrat text-italic", style["mem-motto"])}>{motto}</p>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<Footer className="mt-10 bg-light" />
+		</div>
+	);
 }
