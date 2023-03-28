ee264a41f76e949ea41032bfd8ef2010e5e7737d
TuanLehuskar <tl28072001@gmail.com>
2023-03-28 21:30:37 +0700

    update log

--- src/global/Root.jsx
+++ src/global/Root.jsx
@@ -21,8 +21,6 @@ function Root() {
 		if (isPageLoading) setIsStartedSplash(false);
 	}, [isPageLoading, setIsStartedSplash]);
 
-	console.log(isStartedSplash);
-
 	return (
 		<>
 			<div className={joinCls("mt-5", isPageLoading || !isStartedSplash ? "invisible" : undefined)}>
